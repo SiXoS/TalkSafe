9da12acebda40728a957d108413e52d87d167dce
Simon Lindhen <simon.lindhen@hotmail.com>
2014-02-15 12:25:47 +0100

    Changed to GET protocol for the server, realized some thing is done wrong server side

--- application/src/apicollection/JSONParser.java
+++ application/src/apicollection/JSONParser.java
@@ -8,8 +8,11 @@ import java.util.List;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,10 +41,15 @@ public class JSONParser {
 		try {
 			// defaultHttpClient
 			DefaultHttpClient httpClient = new DefaultHttpClient();
-			HttpPost httpPost = new HttpPost(url);
-			httpPost.setEntity(new UrlEncodedFormEntity(params));
+			
+			StringBuilder requestUrl = new StringBuilder(url);
+			String querystring = URLEncodedUtils.format(params, "utf-8");
+			requestUrl.append("?");
+			requestUrl.append(querystring);
+			
+			HttpGet httpGet = new HttpGet(requestUrl.toString());
 
-			HttpResponse httpResponse = httpClient.execute(httpPost);
+			HttpResponse httpResponse = httpClient.execute(httpGet);
 			HttpEntity httpEntity = httpResponse.getEntity();
 			is = httpEntity.getContent();
 
