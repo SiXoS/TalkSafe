081b90c83a672618e186ad35c0974e31711e01d5
alepers <alex92_persson@hotmail.com>
2014-02-19 11:19:27 +0100

    Start for network code

---
+++ application/src/com/example/talksafe/Connection.java
@@ -0,0 +1,58 @@
+package com.example.talksafe;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Connection implements Runnable {
+
+	private DatagramSocket callListener;
+	private DatagramSocket sender;
+	
+	private final int bufferSize = 128;
+	private boolean isBusy = false;
+	
+	private Connection(int callPort, int listenPort) {
+		try {
+			callListener = new DatagramSocket(listenPort);
+			sender = new DatagramSocket(callPort);
+		}
+		catch(SocketException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		
+		byte[] payload = new byte[bufferSize];
+		DatagramPacket incoming = new DatagramPacket(payload, bufferSize);	
+		try {
+			callListener.receive(incoming);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		isBusy = true;
+		byte[] data = new byte[bufferSize];
+		data = incoming.getData();
+		
+		try {
+			InetAddress targetDevice = InetAddress.getByAddress(data);
+			sender.connect(targetDevice, sender.getPort());
+			sender.send(new DatagramPacket(new byte[bufferSize], bufferSize));
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+	}
+	
+}
