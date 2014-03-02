74b90434bea78e6fe632d035bbf237265fa9d0bf
SiXoS <simon.lindhen@hotmail.com>
2014-03-02 11:57:23 +0100

    ändrade grejer, funkar fortfarande inte

--- application/src/com/example/talksafe/callhandling/SoundFetcher.java
+++ application/src/com/example/talksafe/callhandling/SoundFetcher.java
@@ -1,5 +1,7 @@
 package com.example.talksafe.callhandling;
 
+import java.nio.ByteBuffer;
+
 import com.example.talksafe.callhandling.util.Encrypter;
 import com.example.talksafe.client.ApplicationState;
 
@@ -36,19 +38,25 @@ public class SoundFetcher extends Thread {
 			}
 		}
 		Log.d("Testing play function", "times: " + counter);
-		byte[] temp = new byte[128], temp2 = new byte[115];
+		byte[] temp = new byte[115], temp2 = new byte[115];
+		ByteBuffer buf = ByteBuffer.allocate(115*20);
+		int n=0;
 		ApplicationState state = ApplicationState.getInstance();
 		while(!isInterrupted()) {
-			Log.d("Interrupted", "ne");
 			temp = state.pullIncomingSound();
+			buf.put(temp);
 			//temp2 = decrypter.decrypt(temp);
 			Log.d("SoundFetcher", "playing sound");
-			player.write(temp, 0, temp.length);
+			n++;
+			if(n == 9){
+				Log.d("SoundFetcher", "playing sound");
+				player.write(buf.array(), 0, buf.array().length);
+				buf = (ByteBuffer)buf.clear();
+				n=0;
+			}
 			try {
 				sleep(1);
-			} catch (InterruptedException e) {
-				Log.d("SoundFetcher", e.getMessage());
-			}
+			} catch (InterruptedException e) {			}
 		}
 		player.stop();
 		player.release();
